d964f64dac8309bf54f566d7314ed1babcf52312
Bioinfoplanet <Bioplanet520@outlook.com>
2018-12-26 21:56:15 +0800

    Update step2 hgnc_family_type.r

--- step2 hgnc_family_type.r
+++ step2 hgnc_family_type.r
@@ -1,5 +1,9 @@
 #step2 hgnc_family_type
+#输入文件：hgnc的全表格
+#输出文件：hgnc中基因与family和type的对应关系，两个表格Rdata
 # ftp://ftp.ebi.ac.uk/pub/databases/genenames/new/tsv/hgnc_complete_set.txt
+rm(list = ls())
+if(!require(tidyverse))install.packages('tidyverse')
 library(tidyverse)
 hgnc <- read.csv("hgnc_complete_set.txt",sep = "\t")
 dumd <- function(x){
@@ -13,7 +17,7 @@ dumd <- function(x){
     arrange(desc(count))
   df
 }
-sum_hgnc <- dumd(hgnc)
+sum_hgnc <- dumd(hgnc) #大表格数据统计
 hgnc_family <- hgnc[,c(13,20)]
 hgnc_type <- hgnc[,c(4,20)]
 dup2 <- function(x,m,n){
