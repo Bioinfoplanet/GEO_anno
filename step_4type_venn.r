eb50f52d77fe302843234bf025f16d222c01dff0
Bioinfoplanet <Bioplanet520@outlook.com>
2018-12-28 00:11:22 +0800

    Update step_4type_venn.r

--- step_4type_venn.r
+++ step_4type_venn.r
@@ -10,14 +10,14 @@ count_type <- count(hgnc_type,locus_group,sort = T)
 type_top4 <- as.character((count_type)$locus_group)
 
 Aff_type <- merge(Aff,hgnc_type,
-                    by.x='ensembl_id',
-                    by.y ='ensembl_gene_id')[,-1]
+                  by.x='ensembl_id',
+                  by.y ='ensembl_gene_id')[,-1]
 Bio_type <- merge(Bio,hgnc_type,
-                    by.x='ensembl_id',
-                    by.y ='ensembl_gene_id')[,-1]
+                  by.x='ensembl_id',
+                  by.y ='ensembl_gene_id')[,-1]
 Mine_type <- merge(Mine,hgnc_type,
-                     by.x='ensembl_id',
-                     by.y ='ensembl_gene_id')[,-1]
+                   by.x='ensembl_id',
+                   by.y ='ensembl_gene_id')[,-1]
 
 
 #画图
@@ -25,16 +25,19 @@ venn <- function(x,y,z,name){
   if(!require(VennDiagram))install.packages('VennDiagram')
   library (VennDiagram)
   venn.diagram(x= list(Aff = x,Bio = y,Mine = z),
-               filename = paste0(name,".png"),
-               main = name,
-               height = 450, width = 450,
-               resolution =300,
-               imagetype="png",
-               col="transparent",
-               fill=c("green","yellow","darkorchid1"),
-               alpha = 0.50,
-               cex=0.45,
-               cat.cex=0.45)
+               filename=paste(name,".png"),
+               lwd=1,#圈线粗度
+               lty=1, #圈线类型
+               col=c('#0099CC','#FF6666','#FFCC99'), #圈线颜色
+               fill=c('#0099CC','#FF6666','#FFCC99'), #填充颜色
+               cat.col=c('#0099CC','#FF6666','#FFCC99'),#A和B的颜色
+               cat.cex = 1.5,# A和B的大小
+               rotation.degree = 0,#旋转角度
+               main = name,#主标题内容
+               main.cex = 1.5,#主标题大小
+               cex=1.5,#里面交集字的大小
+               alpha = 0.5,#透明度
+               reverse=TRUE)
 }
 uni <- function(x){
   (unite(x,"x1",c(colnames(x)[1],colnames(x)[2]),sep = " "))[,1]
@@ -55,27 +58,11 @@ for (j in 1:3){
                            locus_group==type_top4[i])
     )$probe_id
   }
-  names(output) <- paste0(1:length(type_top4),type_top4)
+  names(output) <- paste(1:length(type_top4),type_top4,sep = ".")
   output_list[[j]] = output
 }
 names(output_list) <- names(ABM_type_list)
-
-venn <- function(x,y,z,name){
-  if(!require(VennDiagram))install.packages('VennDiagram')
-  library (VennDiagram)
-  venn.diagram(x= list(Aff = x,Bio = y,Mine = z),
-               filename = paste0(name,".png"),
-               main = name,
-               height = 450, width = 450,
-               resolution =300,
-               imagetype="png",
-               col="transparent",
-               fill=c("green","yellow","darkorchid1"),
-               alpha = 0.50,
-               cex=0.45,
-               cat.cex=0.45)
-}
-
+            
 for (i in c(1:length(type_top4))){
   venn(output_list[[1]][[i]],
        output_list[[2]][[i]],
