1a1c590c038bab88a05e432a86f6e127ec242f4b
Bioinfoplanet <Bioplanet520@outlook.com>
2018-12-28 00:10:45 +0800

    Update step3 family_venn.r

--- step3 family_venn.r
+++ step3 family_venn.r
@@ -26,16 +26,19 @@ venn <- function(x,y,z,name){
   if(!require(VennDiagram))install.packages('VennDiagram')
   library (VennDiagram)
   venn.diagram(x= list(Aff = x,Bio = y,Mine = z),
-               filename = paste0(name,".png"),
-               main = name,
-               height = 450, width = 450,
-               resolution =300,
-               imagetype="png",
-               col="transparent",
-               fill=c("green","yellow","darkorchid1"),
-               alpha = 0.50,
-               cex=0.45,
-               cat.cex=0.45)
+               filename=paste(name,".png"),
+               lwd=1,#圈线粗度
+               lty=1, #圈线类型
+               col=c('#0099CC','#FF6666','#FFCC99'), #圈线颜色
+               fill=c('#0099CC','#FF6666','#FFCC99'), #填充颜色
+               cat.col=c('#0099CC','#FF6666','#FFCC99'),#A和B的颜色
+               cat.cex = 1.5,# A和B的大小
+               rotation.degree = 0,#旋转角度
+               main = name,#主标题内容
+               main.cex = 1.5,#主标题大小
+               cex=1.5,#里面交集字的大小
+               alpha = 0.5,#透明度
+               reverse=TRUE)
 }
 uni <- function(x){
   (unite(x,"x1",c(colnames(x)[1],colnames(x)[2]),sep = " "))[,1]
@@ -58,34 +61,15 @@ for (j in 1:3){
                            gene_family==family_top20[i])
     )$probe_id
   }
-  names(output) <- paste0(1:20,family_top20)
+  names(output) <- paste(1:20,family_top20,sep = '.')
   output_list[[j]] = output
 }
 names(output_list) <- names(ABM_family_list)
 
-venn <- function(x,y,z,name){
-  if(!require(VennDiagram))install.packages('VennDiagram')
-  library (VennDiagram)
-  venn.diagram(x= list(Aff = x,Bio = y,Mine = z),
-               filename = paste0(name,".png"),
-               main = name,
-               height = 450, width = 450,
-               resolution =300,
-               imagetype="png",
-               col="transparent",
-               fill=c("green","yellow","darkorchid1"),
-               alpha = 0.50,
-               cex=0.45,
-               cat.cex=0.45)
-}
 
-for (i in c(1:20)){
+for (i in 1:20){
   venn(output_list[[1]][[i]],
        output_list[[2]][[i]],
        output_list[[3]][[i]],
        name = names(output_list[[1]][i]))
 }
-
-
-
-
