c93c6fa0958416eb8eed490e4529e89c6b906735
Bioinfoplanet <Bioplanet520@outlook.com>
2018-12-28 23:58:47 +0800

    Update step_4type_venn.r

--- step_4type_venn.r
+++ step_4type_venn.r
@@ -1,26 +1,26 @@
-#3-1 reshape
+# step4 type作图
 #输入文件：前两步的Rdata
-#输出文件：韦恩图：总和分
+#输出文件：韦恩图
+#4-1 idchange and 4 venn
 rm(list = ls())
 load('hgnc_family_type.Rdata')
 load('ABM_gene_probe.Rdata')
-if(!require(tidyverse))install.packages('tidyverse')
 library(tidyverse)
-count_family <- count(hgnc_family,gene_family,sort = T)
-family_top20 <- as.character((head(count_family,20))$gene_family)
+count_type <- count(hgnc_type,locus_group,sort = T)
+type_top4 <- as.character((count_type)$locus_group)
 
-family_s <- filter(hgnc_family,gene_family %in% family_top20)
-Aff_family <- unique.data.frame(merge(Aff,family_s,
-                    by.x='ensembl_id',
-                    by.y ='ensembl_gene_id')[,-2])
-Bio_family <- unique.data.frame(merge(Bio,family_s,
-                    by.x='ensembl_id',
-                    by.y ='ensembl_gene_id')[,-2])
-Mine_family <- unique.data.frame(merge(Mine,family_s,
-                     by.x='ensembl_id',
-                     by.y ='ensembl_gene_id')[,-2])
+Aff_type <- unique.data.frame(merge(Aff,hgnc_type,
+                  by.x='ensembl_id',
+                  by.y ='ensembl_gene_id')[,-2])
+Bio_type <- unique.data.frame(merge(Bio,hgnc_type,
+                  by.x='ensembl_id',
+                  by.y ='ensembl_gene_id')[,-2])
+Mine_type <- unique.data.frame(merge(Mine,hgnc_type,
+                   by.x='ensembl_id',
+                   by.y ='ensembl_gene_id')[,-2])
+
+
 #画图
-#包装韦恩图函数
 venn <- function(x,y,z,name){
   if(!require(VennDiagram))install.packages('VennDiagram')
   library (VennDiagram)
@@ -43,62 +43,30 @@ venn <- function(x,y,z,name){
 uni <- function(x){
   (unite(x,"x1",c(colnames(x)[1],colnames(x)[2]),sep = " "))[,1]
 }
-venn(uni(Aff_family),uni(Bio_family),uni(Mine_family),"top20_all")
-save(family_top20,Bio_family,Aff_family,Mine_family,file = 'ABM_top20family.Rdata')
+venn(uni(Aff_type),uni(Bio_type),uni(Mine_type),"top4type_all")
+save(type_top4,Bio_type,Aff_type,Mine_type,file = 'ABM_top4type.Rdata')
 
-# 3-2 分开画图
-load('ABM_top20family.Rdata')
-ABM_family_list <- list(Aff_family=Aff_family,Bio_family=Bio_family,Mine_family=Mine_family)
-#需要改掉特殊字符
-family_top20[6] <- 'Small nucleolar RNAs C or D box'
-family_top20[10] <- 'Small nucleolar RNAs H or ACA box'
-#批量作图
+
+# 4-2 分开画图
+load('ABM_top4type.Rdata')
+ABM_type_list <- list(Aff_type=Aff_type,Bio_type=Bio_type,Mine_type=Mine_type)
+
 output_list <- list()
 for (j in 1:3){
   output <- list()
-  for (i in 1 :length(family_top20)){
-    output[[i]] <- (filter(ABM_family_list[[j]],
-                           gene_family==family_top20[i])
+  for (i in 1 :length(type_top4)){
+    output[[i]] <- (filter(ABM_type_list[[j]],
+                           locus_group==type_top4[i])
     )$ensembl_id
   }
-  names(output) <- paste(1:20,family_top20,sep = '.')
+  names(output) <- paste(1:length(type_top4),type_top4,sep = ".")
   output_list[[j]] = output
 }
-names(output_list) <- names(ABM_family_list)
-for (i in 1:20){
+names(output_list) <- names(ABM_type_list)
+            
+for (i in c(1:length(type_top4))){
   venn(output_list[[1]][[i]],
        output_list[[2]][[i]],
        output_list[[3]][[i]],
        name = names(output_list[[1]][i]))
 }
-
-#准备修改的导出为pdf
-#字体有重叠，需要纠正
-#8,9,15,18
-venn2 <- function(x,y,z,name){
-  vennplot <- venn.diagram(x= list(Aff = x,Bio = y,Mine = z),
-               imagetype ="tiff",
-               filename=NULL,
-               lwd=1,#圈线粗度
-               lty=1, #圈线类型
-               col=c('#0099CC','#FF6666','#FFCC99'), #圈线颜色
-               fill=c('#0099CC','#FF6666','#FFCC99'), #填充颜色
-               cat.col=c('#0099CC','#FF6666','#FFCC99'),#A和B的颜色
-               cat.cex = 1.5,# A和B的大小
-               rotation.degree = 0,#旋转角度
-               main = name,#主标题内容
-               main.cex = 1.5,#主标题大小
-               cex=1.5,#里面交集字的大小
-               alpha = 0.5,#透明度
-               reverse=TRUE)
-  grid.draw(vennplot)
-}
-
-dev.off()
-for (i in 18){
-  venn2(output_list[[1]][[i]],
-       output_list[[2]][[i]],
-       output_list[[3]][[i]],
-       name = names(output_list[[1]][i]))
-}
-
